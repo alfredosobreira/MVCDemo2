a887c8264701508b66c9f67a7d88d37cfd15f5b3
Jr <Jr@JRYOGA>
2017-06-19 12:06:53 -0300

    removido comentario

--- MVCDemo2/Startup.cs
+++ MVCDemo2/Startup.cs
@@ -8,7 +8,7 @@ namespace MVCDemo2
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);    //
+            ConfigureAuth(app);  
         }
     }
 }
